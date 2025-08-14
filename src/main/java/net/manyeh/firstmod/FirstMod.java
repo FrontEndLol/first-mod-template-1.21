4533fa1884998a48935a330361101ed502ef21de
Manyeh Luthra <luthra.manyeh@gmail.com>
2025-08-14 14:40:33 -0500

    Added Important Comment

--- src/main/java/net/manyeh/firstmod/FirstMod.java
+++ src/main/java/net/manyeh/firstmod/FirstMod.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Very First Mod Made By Manyeh
+
 public class FirstMod implements ModInitializer {
 	public static final String MOD_ID = "first-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
